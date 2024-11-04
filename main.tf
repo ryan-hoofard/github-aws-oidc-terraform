486a28056f0d15841a6fb17394b81a0877ea732b
ryan-hoofard <ryan.hoofard@arb.ca.gov>
2024-11-04 13:03:16 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "this" {
-  bucket = "ryanh-github-recreated-bucket-delete"
+  bucket = "ryanh-github-recreated-bucket-delete2"
 
   tags = {
     Owner       = "Ryan Hoofard"
